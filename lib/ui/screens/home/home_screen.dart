d3904403fea48e44aec95ac535fcde983fd39bc6
Filip Pajic <fica.pajic@gmail.com>
2019-11-07 20:00:07 +0100

    Added bottom tab navigation to home screen

--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:timestampphy/ui/widgets/bottom_tab_navigation.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,12 +7,55 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  int _selectedTabIndex = 0;
+
+  void _setSelectedTabIndex(index) {
+    setState(() {
+      _selectedTabIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       body: SafeArea(
-        child: Container(),
-      )
+        child: Stack(
+          children: <Widget>[
+            Offstage(
+              offstage: _selectedTabIndex != 0,
+              child: Center(
+                child: Text("Page 1")
+              )
+            ),
+            Offstage(
+                offstage: _selectedTabIndex != 1,
+                child: Center(
+                    child: Text("Page 2")
+                )
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomTabNavigation(
+        notchedShape: CircularNotchedRectangle(),
+        selectedColor: Colors.lightBlue,
+        onTabSelected: _setSelectedTabIndex,
+        items: [
+          BottomTabNavigationItem(iconData: Icons.history, text: "Photo history"),
+          BottomTabNavigationItem(iconData: Icons.search, text: "Photo search")
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+            Icons.camera,
+            color: Colors.lightBlueAccent
+        ),
+        onPressed: () => {/*Will open camera*/},
+        backgroundColor: Colors.white,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
