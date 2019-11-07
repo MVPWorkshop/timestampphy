1a966d294c7438c30005b38b73088517263d61ba
Filip Pajic <fica.pajic@gmail.com>
2019-11-07 18:12:46 +0100

    Fixed package name to comply to naming convention

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:timestampphy/router/Router.dart';
+import 'package:timestampphy/router/router.dart';
 
 class App extends StatelessWidget {
   @override
