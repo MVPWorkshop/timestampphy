3d2efacd2591def45af47be14e6208c9f334995b
Filip Pajic <fica.pajic@gmail.com>
2019-11-15 11:24:07 +0100

    Added Picture importing from the device gallery

--- lib/ui/screens/take_picture/take_picture_screen.dart
+++ lib/ui/screens/take_picture/take_picture_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:timestampphy/core/utils/camera_util.dart';
 import 'package:timestampphy/router/router.dart';
 import 'package:timestampphy/ui/screens/confirm_picture/confirm_picture_screen.dart';
@@ -28,38 +31,69 @@ class TakePictureScreenState extends State<TakePictureScreen> {
     cameraUtil = new CameraUtil();
     await cameraUtil.isCameraInitialized;
 
-    if(!mounted) {
+    if (!mounted) {
       return;
     }
 
     setState(() {});
   }
 
-  void _takePicture() async{
+  void _takePicture() async {
     String tempPicturePath = await cameraUtil.takePicture();
 
-    Navigator.pushNamed(
-        context,
-        Routes.ConfirmPictureScreen,
-        arguments: new ConfirmPictureScreenArgs(picturePath: tempPicturePath)
-    );
+    Navigator.pushNamed(context, Routes.ConfirmPictureScreen,
+        arguments: new ConfirmPictureScreenArgs(picturePath: tempPicturePath));
+  }
+
+  void _pickPicture() async {
+    File picture = await ImagePicker.pickImage(source: ImageSource.gallery);
+
+    Navigator.pushNamed(context, Routes.ConfirmPictureScreen,
+        arguments: new ConfirmPictureScreenArgs(picturePath: picture.path));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder<void>(
-        future: cameraUtil.isCameraInitialized,
-        builder: this._buildCameraPreview,
-      ),
-      backgroundColor: Colors.black,
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.camera, color: Colors.lightBlueAccent),
-        backgroundColor: Colors.white,
-        onPressed: this._takePicture,
-      ),
-    );
+        body: FutureBuilder<void>(
+          future: cameraUtil.isCameraInitialized,
+          builder: this._buildCameraPreview,
+        ),
+        backgroundColor: Colors.black,
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: Container(
+            constraints: BoxConstraints(maxHeight: 100),
+            padding: EdgeInsets.symmetric(vertical: 15, horizontal: 5),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Material(
+                  color: Colors.transparent,
+                  child: InkWell(
+                    onTap: () => Navigator.pop(context),
+                    child: Container(
+                        padding: EdgeInsets.all(10),
+                        child: Icon(Icons.cancel, color: Colors.white)),
+                  ),
+                ),
+                SizedBox(width: 50),
+                FloatingActionButton(
+                  child: Icon(Icons.camera, color: Colors.lightBlueAccent),
+                  backgroundColor: Colors.white,
+                  onPressed: this._takePicture,
+                ),
+                SizedBox(width: 50),
+                Material(
+                  color: Colors.transparent,
+                  child: InkWell(
+                    onTap: () => this._pickPicture(),
+                    child: Container(
+                        padding: EdgeInsets.all(10),
+                        child: Icon(Icons.photo_album, color: Colors.white)),
+                  ),
+                ),
+              ],
+            )));
   }
 
   Widget _buildCameraPreview(context, snapshot) {
@@ -68,10 +102,8 @@ class TakePictureScreenState extends State<TakePictureScreen> {
     if (snapshot.connectionState == ConnectionState.done) {
       return Center(
           child: AspectRatio(
-            child: CameraPreview(cameraController),
-            aspectRatio: cameraController.value.aspectRatio,
-          )
-      );
+              child: CameraPreview(cameraController),
+              aspectRatio: cameraController.value.aspectRatio));
     } else {
       return Center(child: CircularProgressIndicator());
     }
